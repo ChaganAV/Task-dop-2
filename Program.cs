b24583c8119cc36dbdfa1d1eedcc6c3ec2b26390
Alexandr <jwchaganav@gmail.com>
2022-10-19 16:42:55 +0400

    end

--- Program.cs
+++ Program.cs
@@ -3,12 +3,23 @@
 // Когда пользователь вводит exit программа заканчивается
 string stringOut = "exit";
 string? test = String.Empty;
+int count = 0;
+int sumTotal = 0;
 
-Console.WriteLine($"Сложите ваши яблоки по одному, когда яблоки закончатся введите {stringOut}");
+Console.WriteLine($"Сложите ваши яблоки, когда яблоки закончатся введите {stringOut}");
 Console.WriteLine("В конце мы подведем итог");
+
 while(test != stringOut)
 {
     test = Console.ReadLine();
+
+    if (Int32.TryParse(test, out count))
+        sumTotal = sumTotal + count;
+    else
+    {
+        if (test != stringOut)
+            Console.WriteLine("Вводите цифры, пожалуйста!");
+    }
 }
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"Всего {sumTotal} яблок");
