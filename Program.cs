b8836567c13d8c51e29f9b490cbb0c0bd491004b
Alexandr <jwchaganav@gmail.com>
2022-10-19 16:25:51 +0400

    begin

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// программа с беконечным циклом
+// Когда пользователь вводит exit программа заканчивается
+string stringOut = "exit";
+string? test = String.Empty;
+
+Console.WriteLine($"Сложите ваши яблоки по одному, когда яблоки закончатся введите {stringOut}");
+Console.WriteLine("В конце мы подведем итог");
+while(test != stringOut)
+{
+    test = Console.ReadLine();
+}
+
+Console.WriteLine("Hello, World!");
